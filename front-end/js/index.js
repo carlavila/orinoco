8947079195c0c9e64a16e664d7bfdc637a44ac90
carla <vila.carla@yahoo.fr>
2021-05-26 16:11:56 +0200

    Modifications du 26/05/21

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -16,10 +16,75 @@ fetch('http://localhost:3000/api/teddies')
     <div class="products">
       <div class="product">${products[i].name}</div>
       <img class="image" src="${products[i].imageUrl}">
+      <a class="ajout-panier panier" href="#">Ajouter au panier</a>
       <div class="price">${products[i].price/100} €</div>
       <button class="custom-btn"><a href="produits.html?id=${products[i]._id}">Voir Produit</a> </button>
     </div>` ;
   }
+
+  let carts = document.querySelectorAll(".ajout-panier");
+
+  for (let i=0; i < carts.length; i++) {
+        carts[i].addEventListener("click", () => {
+            cartNumbers(products[i]);
+        })
+  }
+    
+  function onLoadCartsNumbers() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+
+    if(productNumbers) {
+      document.querySelector(".nav-item span").textContent = productNumbers;
+    }
+  }
+
+
+  function cartNumbers(products) {
+  
+      let productNumbers = localStorage.getItem("cartNumbers");
+     
+      productNumbers = parseInt(productNumbers);
+
+      if(productNumbers) {
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        document.querySelector(".nav-item span").textContent = productNumbers + 1;
+
+      } else {
+        localStorage.setItem("cartNumbers", 1);
+        document.querySelector(".nav-item span").textContent = 1;
+      }   
+
+      setItems(products);
+  }
+  
+  function setItems(products){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    
+  
+    if(cartItems != null) {
+        
+      if(cartItems[products.tag] == undefined) {
+        cartItems = {
+          ...cartItems,
+          [products.tag]: products
+        }
+      }
+      cartItems[products.tag].inCart += 1;
+  
+    } else {
+      products.inCart = 1;
+      cartItems = {
+          [products.tag]: products
+      }
+    }
+    
+    
+    localStorage.setItem("productsInCart", JSON.stringify
+    (cartItems));
+  
+  }
+  
+  
+  onLoadCartsNumbers();
 });
-
-
--- front-end/js/produits.js
+++ front-end/js/produits.js
@@ -1,4 +1,25 @@
 
+// Fonction pour la création du tableau dans le localStorage
+/* si l'onglet est fermé ou rafraichit, on perd les données d'un formulaire*/
+/* Maintient une zone de stockage distinct */
+
+const items = ()=> {
+
+  let total = 0; // Créations des variables
+  let items = [];
+  
+    let teddy = JSON.stringify(items); // Enregistrement dans le localStorage, convertit une valeur JavaScript en chaîne JSON.
+    localStorage.setItem ("teddy", teddy);
+    localStorage.setItem ("total", total);
+  
+  }
+  
+  if(localStorage.teddy == undefined){
+  
+    items();
+  
+  }
+  
 
 //Récupère lien get
 let link = location.href;
@@ -68,6 +89,7 @@ fetch('http://localhost:3000/api/teddies')
       const error = document.querySelector('.error');
       error.innerHTML = "Veuillez sélectionner une couleur";
     } else{
+
 
 
       // Récupération du tableau dans le localStorage
